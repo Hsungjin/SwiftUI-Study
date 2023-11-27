481086d9c24ecccc2d9d1de2098d79548636c828
Hsungjin <hsungjin142@gmail.com>
2023-11-28 02:21:55 +0900

    Create: Hike - ClonCoding App

---
+++ Udemy/Hike/Hike/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Hike
+//
+//  Created by 황성진 on 11/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Image("image-1")
+            .resizable()
+            .scaledToFit()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Udemy/Hike/Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by 황성진 on 11/28/23.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
