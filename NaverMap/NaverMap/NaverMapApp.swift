2c55ef54dd5510a2056f38b292e0b5f7cdf43d7b
Hsungjin <hsungjin142@gmail.com>
2023-12-27 10:30:22 +0900

    [Add] NaverMap 추가

---
+++ NaverMap/NaverMap/NaverMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaverMapApp.swift
+//  NaverMap
+//
+//  Created by 황성진 on 12/27/23.
+//
+
+import SwiftUI
+
+@main
+struct NaverMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
