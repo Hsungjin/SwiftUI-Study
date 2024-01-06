e27420ad53c784da8b4d1212a9c0dd0e05db5808
Hsungjin <hsungjin142@gmail.com>
2024-01-06 20:18:35 +0900

    [Add] Devote 클론코딩 시작

---
+++ Udemy/Devote/Devote/DevoteApp.swift
@@ -0,0 +1,20 @@
+//
+//  DevoteApp.swift
+//  Devote
+//
+//  Created by 황성진 on 1/6/24.
+//
+
+import SwiftUI
+
+@main
+struct DevoteApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
