42e268a22907ae340e8be57bf8cbee841424c0c7
Hsungjin <hsungjin142@gmail.com>
2024-03-14 01:43:49 +0900

    [Add] 타이머 기능, URL 세션 추가

--- TCA_tutorial/TCA_tutorial/CounterView.swift
+++ TCA_tutorial/TCA_tutorial/CounterView.swift
@@ -36,6 +36,41 @@ struct CounterView: View {
                 .background(Color.black.opacity(0.1))
                 .cornerRadius(10)
             }
+            
+            Button(store.isTimerRunning ? "Stop timer" : "Start timer") {
+              store.send(.toggleTimerButtonTapped)
+            }
+            .font(.largeTitle)
+            .padding()
+            .background(Color.black.opacity(0.1))
+            .cornerRadius(10)
+            
+            Button {
+                store.send(.resetTimer)
+            } label: {
+                Text("ResetTimer")
+            }
+            .font(.largeTitle)
+            .padding()
+            .background(Color.black.opacity(0.1))
+            .cornerRadius(10)
+            
+            Button("Fact") {
+              store.send(.factButtonTapped)
+            }
+            .font(.largeTitle)
+            .padding()
+            .background(Color.black.opacity(0.1))
+            .cornerRadius(10)
+            
+            if store.isLoading {
+                ProgressView()
+            } else if let fact = store.fact {
+                Text(fact)
+                    .font(.largeTitle)
+                    .multilineTextAlignment(.center)
+                    .padding()
+            }
         }
     }
 }
